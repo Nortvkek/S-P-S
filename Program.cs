998b4e3bd2dda28e2d317d2f295f0175ee4251df
User <User@DESKTOP-015TVHK>
2025-12-18 00:17:09 +0700

    added player and bot score, that saves to file

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
-﻿namespace ConsoleApp22
+﻿using System.Reflection.PortableExecutable;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp22
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static public int yourPts = 0, nYourPts = 0;
+        static async Task Main(string[] args)
         {
+            string path = "points.txt";
+            await GetPointsFromFileAsync(path);
+            PrintPts();
             string? your_choice = Console.ReadLine();
             Console.WriteLine($"Ваш выбор: {your_choice}");
             string? not_your_choice = Play();
             Console.WriteLine($"Выбор оппонента: {not_your_choice}");
             CheckResults(your_choice, not_your_choice);
+            PrintPts();
+            await AddPointsToFileAsync(path);
         }
 
 
@@ -22,11 +31,43 @@
         static void CheckResults(string? yourStr, string? othStr)
         {
             if ((yourStr.ToLower() == "stone" && othStr == "scissors") || (yourStr.ToLower() == "scissors" && othStr == "paper") || (yourStr.ToLower() == "paper" && othStr == "stone"))
+            {
                 Console.WriteLine("Вы победили!");
+                yourPts += 1;
+            }
             if ((othStr.ToLower() == "stone" && yourStr == "scissors") || (othStr.ToLower() == "scissors" && yourStr == "paper") || (othStr.ToLower() == "paper" && yourStr == "stone"))
+            {
                 Console.WriteLine("Вы проиграли!");
+                nYourPts += 1;
+            }
             else if (yourStr == othStr)
                 Console.WriteLine("Ничья!");
+        }
+
+        static async Task GetPointsFromFileAsync(string path)
+        {
+            string[] pts;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string text = await reader.ReadToEndAsync();
+                pts = text.Split(new char[] { ' ' });
+                yourPts = Convert.ToInt32(pts[0]);
+                nYourPts = Convert.ToInt32(pts[1]);
+            }
+        }
+
+        static async Task AddPointsToFileAsync(string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                await writer.WriteLineAsync($"{yourPts} {nYourPts}");
+            }
+        }
+
+        static void PrintPts()
+        {
+            Console.WriteLine($"Ваши очки: {yourPts}");
+            Console.WriteLine($"Очки оппонента: {nYourPts}");
         }
 
     }
