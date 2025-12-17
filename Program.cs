ac56785809401456eba5b9f10b943d960881db90
User <User@DESKTOP-015TVHK>
2025-12-17 18:11:29 +0700

    added end-of-game conditions

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine($"Ваш выбор: {your_choice}");
             string? not_your_choice = Play();
             Console.WriteLine($"Выбор оппонента: {not_your_choice}");
+            CheckResults(your_choice, not_your_choice);
         }
 
 
@@ -16,6 +17,16 @@
             Random rand = new Random();
             string[] variants = {"stone", "scissors", "paper"};
             return variants[rand.Next(0, variants.Length)];
+        }
+
+        static void CheckResults(string? yourStr, string? othStr)
+        {
+            if ((yourStr.ToLower() == "stone" && othStr == "scissors") || (yourStr.ToLower() == "scissors" && othStr == "paper") || (yourStr.ToLower() == "paper" && othStr == "stone"))
+                Console.WriteLine("Вы победили!");
+            if ((othStr.ToLower() == "stone" && yourStr == "scissors") || (othStr.ToLower() == "scissors" && yourStr == "paper") || (othStr.ToLower() == "paper" && yourStr == "stone"))
+                Console.WriteLine("Вы проиграли!");
+            else if (yourStr == othStr)
+                Console.WriteLine("Ничья!");
         }
 
     }
