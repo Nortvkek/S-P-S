6d639c42ac26117f7ef677104a89de5aafb3ff1e
User <User@DESKTOP-015TVHK>
2025-12-17 18:07:55 +0700

    added player and opponent chioce

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string? your_choice = Console.ReadLine();
+            Console.WriteLine($"Ваш выбор: {your_choice}");
+            string? not_your_choice = Play();
+            Console.WriteLine($"Выбор оппонента: {not_your_choice}");
         }
+
+
+        static string Play()
+        {
+            Random rand = new Random();
+            string[] variants = {"stone", "scissors", "paper"};
+            return variants[rand.Next(0, variants.Length)];
+        }
+
     }
 }
